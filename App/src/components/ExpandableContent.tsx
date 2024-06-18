2f3f89ae6ed65b45def53cec0e37c125a5af8aa6
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2024-06-18 16:35:35 +0200

    10 request influxdb api (#12)
    * setup
    
    * android emulator setup
    
    * added an app navigator and 2 initial screens
    
    * rename screen
    
    * simple hello world button added
    
    * added api.ts with a simple get request that gets all readings from the topic mdma/1481765933
    
    * enhanced api.ts
    
    * added a scrollable list component for better visualisation
    
    * added button for fetching only the last new measurement
    
    * updated api token
    
    * added new Screen and Button
    
    * created a better bar
    
    * better styling for the expanding card area
    
    * split HorizontalBar components into 2 new classes
    
    * displaying now the last value of the sensor
    
    * added a percentage number to the bar
    
    * added toast
    
    ---------
    
    Co-authored-by: NickMittmann <nmittmann@stud.hs-heilbronn.de>
    Co-authored-by: Julianertle <julian_ertle@web.de>

---
+++ App/src/components/ExpandableContent.tsx
@@ -0,0 +1,130 @@
+import React, { useRef, useEffect } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
+import Svg, { Text } from 'react-native-svg';
+import { Measurement } from '../services/Measurement'; // Adjust the path as per your project structure
+
+interface ExpandableContentProps {
+    expanded: boolean;
+    measurement?: Measurement; // Accepts Measurement instance as optional prop
+}
+
+const ExpandableContent: React.FC<ExpandableContentProps> = ({ expanded, measurement }) => {
+    const expandAnim = useRef(new Animated.Value(expanded ? 1 : 0)).current;
+
+    useEffect(() => {
+        Animated.timing(expandAnim, {
+            toValue: expanded ? 1 : 0,
+            duration: 300,
+            useNativeDriver: false,
+        }).start();
+    }, [expanded]);
+
+    const expandableHeight = expandAnim.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 200], // Adjust maximum height of the expandable content
+    });
+
+    const expandableOpacity = expandAnim.interpolate({
+        inputRange: [0, 0.5, 1],
+        outputRange: [0, 0, 1],
+    });
+
+    const displayValue = (value: any) => {
+        return value !== undefined && value !== null ? value : 'undefined';
+    };
+
+    const formatTime = (time: string) => {
+        const date = new Date(time);
+        const formattedDate = date.toLocaleDateString('en-GB'); // European date format (DD/MM/YYYY)
+        const formattedTime = date.toLocaleTimeString('en-GB', { hour12: false }); // Time with seconds
+        return `${formattedDate} ${formattedTime}`;
+    };
+
+    if (!measurement) {
+        return (
+            <Animated.View
+                style={[
+                    styles.expandableContent,
+                    {
+                        height: expandableHeight,
+                        opacity: expandableOpacity,
+                    },
+                ]}
+            >
+                <Svg height={200} width="100%">
+                    <Text x="5%" y="50%" fontSize="16" fontWeight="bold" fill="black" textAnchor="start">
+                        Measurement Data Undefined
+                    </Text>
+                </Svg>
+            </Animated.View>
+        );
+    }
+
+    return (
+        <Animated.View
+            style={[
+                styles.expandableContent,
+                {
+                    height: expandableHeight,
+                    opacity: expandableOpacity,
+                },
+            ]}
+        >
+            <Svg height={200} width="100%">
+                {/* Time */}
+                <Text x="5%" y="20%" fontSize="16" fontWeight="bold" fill="black" textAnchor="start">
+                    Time:
+                </Text>
+                <Text x="35%" y="20%" fontSize="16" fill="black" textAnchor="start">
+                    {displayValue(formatTime(measurement.time))}
+                </Text>
+
+                {/* Host */}
+                <Text x="5%" y="35%" fontSize="16" fontWeight="bold" fill="black" textAnchor="start">
+                    Host:
+                </Text>
+                <Text x="35%" y="35%" fontSize="16" fill="black" textAnchor="start">
+                    {displayValue(measurement.host)}
+                </Text>
+
+                {/* Topic */}
+                <Text x="5%" y="50%" fontSize="16" fontWeight="bold" fill="black" textAnchor="start">
+                    Topic:
+                </Text>
+                <Text x="35%" y="50%" fontSize="16" fill="black" textAnchor="start">
+                    {displayValue(measurement.topic)}
+                </Text>
+
+                {/* Available Space */}
+                <Text x="5%" y="65%" fontSize="16" fontWeight="bold" fill="black" textAnchor="start">
+                    Bin Size:
+                </Text>
+                <Text x="35%" y="65%" fontSize="16" fill="black" textAnchor="start">
+                    100 cm
+                </Text>
+
+                {/* Level */}
+                <Text x="5%" y="80%" fontSize="16" fontWeight="bold" fill="black" textAnchor="start">
+                    Room Left:
+                </Text>
+                <Text x="35%" y="80%" fontSize="16" fill="black" textAnchor="start">
+                    {displayValue(measurement.data)} cm
+                </Text>
+            </Svg>
+        </Animated.View>
+    );
+};
+
+const styles = StyleSheet.create({
+    expandableContent: {
+        overflow: 'hidden',
+        marginTop: 5,
+        marginLeft: 10,
+        padding: 10,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        position: 'relative',
+    },
+});
+
+export default ExpandableContent;
---
+++ App/src/services/api.ts
@@ -0,0 +1,91 @@
+import { Measurement } from './Measurement';
+const INFLUXDB_URL = 'http://10.0.2.2:8086/query'; // When you run the project on an Android device, localhost is pointing to ur computer instead of the Android device so I changed http://localhost:3030 to http://10.0.2.2:3030
+const DATABASE_NAME = 'iot_data';
+const API_TOKEN = 'nbFDQnczXNoX0j-fCgioSybCP33D_n1WnAJiR8bexMEue1ww-4-z5LYKTK8g9bXZf5OhXY4v3c7vJRSsTBJoSg==';
+
+export const fetchLastMinutesFromInfluxDB = async (topic: string, minutes: number): Promise<Measurement[]> => {
+  try {
+    // Calculate the time threshold for the last 'minutes'
+    const now = new Date();
+    const startTime = new Date(now.getTime() - minutes * 60000).toISOString(); // Convert minutes to milliseconds
+
+    const query = `SELECT * FROM "mqtt_consumer" WHERE "topic" = '${topic}' AND time > '${startTime}'`;
+    
+
+    const response = await fetch(`${INFLUXDB_URL}?pretty=true&db=${DATABASE_NAME}&q=${encodeURIComponent(query)}`, {
+      headers: {
+        'Authorization': `Token ${API_TOKEN}`,
+        'Accept': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (!data.results || !data.results[0].series || !data.results[0].series[0].values) {
+      throw new Error('No data available for the specified topic and time range.');
+    }
+
+    const values = data.results[0].series[0].values;
+    const measurementList: Measurement[] = [];
+
+    for (let i = 0; i < values.length; i++) {
+      const valueArray = values[i];
+      const value = valueArray[1];
+      const time = valueArray[0];
+      const host = valueArray[2];
+      const topic = valueArray[3];
+
+      const measurement = new Measurement(time, value, host, topic);
+      measurementList.push(measurement);
+    }
+
+    return measurementList;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+
+export const fetchNewestValueFromInfluxDB = async (topic: string): Promise<Measurement | null> => {
+  try {
+    const query = `SELECT * FROM "mqtt_consumer" WHERE "topic" = '${topic}' ORDER BY time DESC LIMIT 1`;
+
+    const response = await fetch(`${INFLUXDB_URL}?pretty=true&db=${DATABASE_NAME}&q=${encodeURIComponent(query)}`, {
+      headers: {
+        'Authorization': `Token ${API_TOKEN}`,
+        'Accept': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (!data.results || !data.results[0].series || !data.results[0].series[0].values || data.results[0].series[0].values.length === 0) {
+      return null; // No data available for the specified topic
+    }
+
+    const valueArray = data.results[0].series[0].values[0];
+    const value = valueArray[1];
+    const time = valueArray[0];
+    const host = valueArray[2]; // Assuming you have host and topic in your InfluxDB measurement
+    const fetchedTopic = valueArray[3];
+
+    if (fetchedTopic !== topic) {
+      throw new Error(`Unexpected topic '${fetchedTopic}' fetched from InfluxDB.`);
+    }
+
+    const measurement = new Measurement(time, value, host, topic);
+    return measurement;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
