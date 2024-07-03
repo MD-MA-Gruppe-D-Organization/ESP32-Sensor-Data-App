656dccefc1b77f578c23206c36207f7dbbcd04bd
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2024-07-04 00:04:26 +0200

    13 measurement settings (#15)
    * feat: setup expo
    
    * feat: add parameters
    
    * feat: save metadata in storage
    
    * feat: add loading indicator
    
    * chore: add todo
    
    * chore: adjust readme
    
    * 11 display multiple sensors (#14)
    
    * debugger setup, moving gitignore to root directory, changing api token
    
    * added api method fetchAllTopicsFromInfluxDB
    
    * showing multiple sensor cards in app
    
    * showing hotname as location if no location is selected
    
    * saving and loading locations out of storage
    
    * loading bin size from storage now
    
    ---------
    
    Co-authored-by: julianertle <111023888+julianertle@users.noreply.github.com>

---
+++ Expo/components/SensorCard.tsx
@@ -0,0 +1,215 @@
+import { Measurement } from "@/api";
+import React, { useState } from "react";
+import {
+  Card,
+  IconButton,
+  ProgressBar,
+  useTheme,
+  DataTable,
+  List,
+  Button,
+  Dialog,
+  Portal,
+  TextInput,
+  Tooltip,
+  ActivityIndicator,
+} from "react-native-paper";
+import { ThemedText } from "./ThemedText";
+import Storage from "react-native-storage";
+
+interface SensorCardProps {
+  measurement: Measurement | undefined;
+  handleRefresh: () => void;
+  index: number;
+  onEdit: (binSize: number | undefined, location: string | undefined, hostName: string | undefined) => void;
+  binSize: number | undefined;
+  location: string | undefined;
+  hostName: string;
+  loadingRefresh: boolean;
+}
+
+const SensorCard: React.FC<SensorCardProps> = ({
+  measurement,
+  handleRefresh,
+  index,
+  onEdit,
+  binSize,
+  location,
+  hostName,
+  loadingRefresh,
+}) => {
+  const theme = useTheme();
+  const [visible, setVisible] = useState(false);
+  const [editedBinSize, setEditedBinSize] = useState(binSize);
+
+  const [editedLocation, setEditedLocation] = useState(location);
+
+  const handleEdit = () => {
+    setVisible(true);
+  };
+
+  const handleSave = () => {
+    if (editedBinSize !== undefined && editedLocation !== undefined) {
+      onEdit(editedBinSize, editedLocation,measurement?.metaData.hostName);
+      setVisible(false);
+    } else {
+      console.error("save error");
+    }
+  };
+
+  function normalizeValue(currentValue: number, maxValue = 100) {
+    return currentValue / maxValue;
+  }
+  const progressColor = (progress: number) => {
+    const r = Math.floor((255 * progress) / 100);
+    const g = Math.floor((255 * (100 - progress)) / 100);
+    return `rgb(${r},${g},0)`;
+  };
+  return (
+    <Card
+      key={index}
+      style={{
+        backgroundColor: theme.colors.surface,
+        shadowColor: theme.colors.shadow,
+        borderColor: theme.colors.outline,
+        borderWidth: 0.5,
+        marginBottom: 16,
+      }}
+    >
+      <Card.Content
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "baseline",
+          flexDirection: "row",
+        }}
+      >
+        <ThemedText type="subtitle">
+          {location ? location : measurement?.metaData.hostName}
+        </ThemedText>
+        <Tooltip title="current fill-level">
+          <ThemedText type="subtitle">
+            {(
+              normalizeValue(
+                measurement?.influx.value ?? 0,
+                measurement?.metaData.binSize
+              ) * 100
+            ).toFixed(1)}
+            %
+          </ThemedText>
+        </Tooltip>
+        {loadingRefresh ? (
+          <ActivityIndicator size="small" />
+        ) : (
+          <IconButton
+            icon="reload"
+            onPress={() => {
+              handleRefresh();
+            }}
+          />
+        )}
+      </Card.Content>
+      <Card.Content>
+        <ProgressBar
+          progress={normalizeValue(
+            measurement?.influx.value ?? 0,
+            measurement?.metaData.binSize
+          )}
+          color={progressColor(
+            measurement?.influx.value ??
+              0 *
+                normalizeValue(
+                  measurement?.influx.value ?? 0,
+                  measurement?.metaData.binSize
+                )
+          )}
+          style={{ marginTop: 16, height: 20, borderRadius: 8 }}
+        />
+        <List.Accordion
+          title=""
+          right={(props) => (
+            <List.Icon
+              {...props}
+              icon="information"
+              color={theme.colors.backdrop}
+            />
+          )}
+          style={{ borderRadius: 8, marginTop: 8 }}
+        >
+          <DataTable>
+            <DataTable.Row>
+              <DataTable.Cell textStyle={{ fontWeight: "bold" }}>
+                Time
+              </DataTable.Cell>
+              <DataTable.Cell>{measurement?.influx.time}</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell textStyle={{ fontWeight: "bold" }}>
+                Host
+              </DataTable.Cell>
+              <DataTable.Cell>{measurement?.influx.host}</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell textStyle={{ fontWeight: "bold" }}>
+                Topic
+              </DataTable.Cell>
+              <DataTable.Cell>{measurement?.influx.topic}</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell textStyle={{ fontWeight: "bold" }}>
+                Bin Size
+              </DataTable.Cell>
+              <DataTable.Cell>
+                {measurement?.metaData.binSize} cm
+              </DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell textStyle={{ fontWeight: "bold" }}>
+                Available Space
+              </DataTable.Cell>
+              <DataTable.Cell>
+                {(
+                  100 -
+                  normalizeValue(
+                    measurement?.influx.value ?? 0,
+                    measurement?.metaData.binSize
+                  ) *
+                    100
+                ).toFixed(1)}
+                %
+              </DataTable.Cell>
+            </DataTable.Row>
+          </DataTable>
+        </List.Accordion>
+      </Card.Content>
+      <IconButton icon="cog" onPress={handleEdit} />
+      <Portal>
+        <Dialog
+          visible={visible}
+          onDismiss={() => [setVisible(false), handleSave]}
+        >
+          <Dialog.Title>Settings</Dialog.Title>
+          <Dialog.Content style={{ display: "flex", gap: 8 }}>
+            <TextInput
+              label="Bin Size"
+              mode="outlined"
+              value={editedBinSize?.toString()}
+              onChangeText={(text) => setEditedBinSize(Number(text))}
+            />
+            <TextInput
+              label="Location"
+              mode="outlined"
+              value={editedLocation}
+              onChangeText={(text) => setEditedLocation(text)}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={handleSave}>Save</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </Card>
+  );
+};
+
+export default SensorCard;
